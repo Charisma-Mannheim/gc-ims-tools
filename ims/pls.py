8212ea465f7f60adfaf619528ec506093e238a67
Joscha Christmann <joscha.christmann@gmail.com>
2021-07-08 14:35:52 +0200

    Reworked optimization plot and vips

--- ims/pls.py
+++ ims/pls.py
@@ -13,10 +13,10 @@ from ims import BaseModel
 
 
 class PLSR(BaseModel):
-    
+
     def __init__(self, dataset, scaling_method=None, optimize=True,
                  n_components=20, kfold=5, **kwargs):
-        
+
         super().__init__(dataset, scaling_method)
         self.kfold = kfold
         self.n_components = n_components
@@ -26,36 +26,45 @@ class PLSR(BaseModel):
             # _accuracies is needed for the optimization plot
             self._best_comp, self._accuracies = self._optimize_pls()
 
+        self._fit(**kwargs)
+
+    def _fit(self, **kwargs):
         self._pls = PLSRegression(n_components=self._best_comp, **kwargs)
         self._pls.fit(self.X, self.y)
         
         self.prediction = self._pls.predict(self.X)
-        self.r2_score = round(r2_score(self.y, self.prediction), 3)
-        self.accuracy = round(mean_squared_error(self.y, self.prediction), 3)
+        self.r2_score = round(r2_score(self.y, self.prediction), 2)
+        self.mse = round(mean_squared_error(self.y, self.prediction), 2)
         
-        self.prediction_cv = cross_val_predict(self._pls, self.X, self.y, cv=kfold)
-        self.r2_score_cv = round(r2_score(self.y, self.prediction_cv), 3)
-        self.mse_cv = round(mean_squared_error(self.y, self.prediction_cv), 3)
+        self.prediction_cv = cross_val_predict(self._pls, self.X, self.y, cv=self.kfold)
+        self.r2_score_cv = round(r2_score(self.y, self.prediction_cv), 2)
+        self.mse_cv = round(mean_squared_error(self.y, self.prediction_cv), 2)
         
         self.result = {
             "r2 score": self.r2_score,
-            "accuracy": self.accuracy,
+            "mse": self.accuracy,
             "r2 score cv": self.r2_score_cv,
-            "accuracy cv": self.mse_cv
+            "mse cv": self.mse_cv
         }
 
     def _optimize_pls(self):
-        accuracy = []
+        mse = []
         component = np.arange(1, self.n_components)
         
         for i in component:
             pls = PLSRegression(n_components=i)
             y_cv = cross_val_predict(pls, self.X, self.y, cv=self.kfold)
-            accuracy.append(mean_squared_error(self.y, y_cv))
+            mse.append(mean_squared_error(self.y, y_cv))
             
-        best_ac = np.argmin(accuracy)
-        return component[best_ac], accuracy
-        
+        best_ac = np.argmin(mse)
+        return component[best_ac], mse
+    
+    def calc_vip_scores(self):
+        pass
+    
+    def plot_vip_scores(self):
+        pass
+    
     def plot_optimization(self):
         component = np.arange(1, self.n_components)
         best_ac = np.argmin(self._accuracies)
@@ -84,6 +93,9 @@ class PLSR(BaseModel):
             plt.title("Crossvalidation")
             plt.legend(frameon=True, fancybox=True, facecolor="white")
         return fig
+    
+    def plot_coefficients(self):
+        pass
 
 
 class PLS_DA(BaseModel):
@@ -128,7 +140,8 @@ class PLS_DA(BaseModel):
         # either iterates from 1 to n_components and finds best accuracy
         # or uses n_components as the number of components directly 
         if self.optimize:
-            self._best_comp, self._accuracies = self._optimize_plsda()
+            self._best_comp, self._accuracies, self._precisions,\
+                self._recalls = self._optimize_plsda()
             self._fit(self._best_comp)
             self.accuracy, self.precision, self.recall = self._crossval(self._best_comp)
         else:    
@@ -179,14 +192,17 @@ class PLS_DA(BaseModel):
         """Optimizes number of components"""
         component = np.arange(1, self.n_components + 1)
         accuracy = []
+        precision = []
+        recall = []
         for i in component:
-            ac, _, _ = self._crossval(i)
+            ac, pre, re = self._crossval(i)
             accuracy.append(ac)
+            precision.append(pre)
+            recall.append(re)
             
         best_ac = np.argmax(accuracy)
-        return component[best_ac], accuracy
-    
-    
+        return (component[best_ac], accuracy, precision, recall)
+
     def _fit(self, n_comps):
         self._pls = PLSRegression(n_comps)
         self._pls.fit(self.X, self.y_binary)
@@ -201,7 +217,7 @@ class PLS_DA(BaseModel):
             self.x_loadings = self._pls.x_loadings_
         else:
             self.x_loadings = self._pls.x_loadings_ / self.weights[:, None]
-    
+
     def _get_top_coef_indices(self, n):
         indices = []
         for i in range(len(self.groups)):
@@ -212,8 +228,12 @@ class PLS_DA(BaseModel):
 
         indices = np.sort(np.array(indices).flatten())
         return np.unique(indices)
-            
-    def calc_vip_scores(self, top_n_coeff=None):
+    
+    def plot_confusion_matrix(self):
+        pass
+    
+    
+    def calc_vip_scores(self, top_n_coeff=None, threshold=None):
         """
         Calculates variable importance in projection (VIP) scores.
         Optionally only of features with high coefficients.
@@ -223,34 +243,34 @@ class PLS_DA(BaseModel):
         top_n_coeff : int, optional
             Number highest coefficients per group
             if None calculates all, by default None
+            
+        threshold : int, optional
+            if given keeps only VIP scores greater than threshold,
+            by default None
 
         Returns
         -------
         numpy.ndarray
         """
         if top_n_coeff is None:
-            w = self.x_weights
+            xw = self.x_weights
         else:
             self._indices = self._get_top_coef_indices(top_n_coeff)
-            w = self.x_weights[self._indices, :]
+            xw = self.x_weights[self._indices, :]
 
-        t = self.x_scores
-        q = self.y_loadings
+        xs = self.x_scores
+        yl = self.y_loadings
 
-        p, h = w.shape
-        
-        vips = np.zeros((p,))
-
-        s = np.diag(t.T @ t @ q.T @ q).reshape(h, -1)
-        total_s = np.sum(s)
-
-        for i in range(p):
-            weight = np.array([(w[i, j] / np.linalg.norm(w[:, j]))**2 for j in range(h)])
-            vips[i] = np.sqrt(p * (s.T @ weight)/total_s)
+        vips = _vip_scores(xw, xs, yl)
 
         self.vip_scores = vips
-        return vips
+        
+        if threshold is not None:
+            i = np.where(self.vip_scores > threshold)
+            self.vip_scores = self.vip_scores[i]
+            self._indices = self._indices[i]
 
+        return vips
 
     def plot(self, x_comp=1, y_comp=2, annotate=False):
         """
@@ -271,8 +291,11 @@ class PLS_DA(BaseModel):
         Returns
         -------
         matplotlib.figure.Figure
-        """        
-        cols = [f"PLS Component {i}" for i in range(1, self.n_components + 1)]
+        """
+        if self.optimize:
+            cols = [f"PLS Component {i}" for i in range(1, self._best_comp + 1)]
+        else:
+            cols = [f"PLS Component {i}" for i in range(1, self.n_components + 1)]
         df = pd.DataFrame(self.x_scores, columns=cols)
         df["Group"] = self.dataset.labels
         df["Sample"] = self.dataset.samples
@@ -299,7 +322,7 @@ class PLS_DA(BaseModel):
 
         return fig
 
-    def plot_optimisation(self):
+    def plot_optimization(self):
         """
         Plots accuracy from crossvalidation vs number of components
         to find the best parameter.
@@ -320,19 +343,29 @@ class PLS_DA(BaseModel):
         best_ac = np.argmax(self._accuracies)
         
         with plt.style.context("seaborn"):
-            fig = plt.figure()
-            plt.plot(component, self._accuracies)
-            plt.scatter(component, self._accuracies)
-            plt.plot(
+            fig, axs = plt.subplots(3, figsize=(8, 12))
+
+            axs[0].plot(component, self._accuracies)
+            axs[0].scatter(component, self._accuracies) 
+            axs[0].plot(
                 component[best_ac],
                 self._accuracies[best_ac],
                 color="tab:orange",
                 marker="*",
                 markersize=20
                 )
-            plt.xlabel("Number of PLS Components")
-            plt.ylabel("Accuracy")
-            plt.title("PLS-DA")
+            axs[0].set_title("PLS-DA Optimization", fontsize=14)
+            axs[0].set_ylabel("Accuracy")
+
+            axs[1].plot(component, self._precisions)
+            axs[1].scatter(component, self._precisions)
+            axs[1].set_ylabel("Precision", fontsize=12)
+
+            axs[2].plot(component, self._recalls)
+            axs[2].scatter(component, self._recalls)
+            axs[2].set_xlabel("Number of PLS components", fontsize=12)
+            axs[2].set_ylabel("Recall", fontsize=12)
+
         return fig
 
     def plot_coefficients(self, group=0):
@@ -400,7 +433,7 @@ class PLS_DA(BaseModel):
         ValueError
             If VIP scores are not calculated.
         """        
-        if not hasattr(self.vip_scores):
+        if not hasattr(self, "vip_scores"):
             raise ValueError("Must calculate VIP scores first.")
         
         vip_matrix = np.zeros(self.X.shape[1])
@@ -432,3 +465,43 @@ class PLS_DA(BaseModel):
         ax.yaxis.set_minor_locator(AutoMinorLocator())
         
         return fig
+    
+
+def _vip_scores(xw, xs, yl):
+    """
+    Calculates VIP scores of PLS x_weights, x_scores
+    and y_loadings.
+
+    Parameters
+    ----------
+    xw : numpy.ndarray
+        x_weights
+
+    xs : numpy.ndarray
+        x_scores
+
+    yl : numpy.ndarray
+        y_loadings
+
+    Returns
+    -------
+    numpy.ndarray
+        Vector of VIP scores
+    """    
+    p, h = xw.shape
+
+    vips = np.zeros((p,))
+    weight = np.zeros((h,))
+
+    s = np.diag(xs.T @ xs @ yl.T @ yl)
+    s = s.reshape(h, -1)
+
+    total_s = np.sum(s)
+
+    for i in range(p):
+        for j in range(h):
+            weight[j] = (xw[i, j] / np.linalg.norm(xw[:, j]))**2
+
+        vips[i] = np.sqrt(p * (s.T @ weight) / total_s)
+
+    return vips
