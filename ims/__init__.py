96307bbdc91279014d8064870673f527577293ee
Joscha Christmann <joscha.christmann@gmail.com>
2021-09-23 10:30:22 +0200

    Removed relative imports

--- ims/__init__.py
+++ ims/__init__.py
@@ -1,6 +1,6 @@
-from .gcims import *
-from .dataset import *
-from .basemodel import *
-from .generalmodels import *
-from .pca import *
-from .pls import *
+from ims.gcims import Spectrum
+from ims.dataset import Dataset
+from ims.basemodel import BaseModel
+from ims.generalmodels import Classification, Regression
+from ims.pca import PCA_Model
+from ims.pls import PLSR, PLS_DA
