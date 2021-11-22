0e91f822b4d2d7ded6656ae37f8cf1b4523a031a
joscha-christmann <joscha.christmann@gmail.com>
2021-11-22 13:04:20 +0100

    Updated packaging

--- setup.py
+++ setup.py
@@ -5,26 +5,26 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 setuptools.setup(
-    name="ims",
+    name="gc-ims-tools",
     version="0.1.0",
-    url="https://github.com/Charisma-Mannheim/ims",
+    url="https://github.com/Charisma-Mannheim/gc-ims-tools",
     author="Joscha Christmann",
     author_email="j.christmann@hs-mannheim.de",
     description="Analyze GC-IMS data.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
-    license="LICENSE.txt",
+    license="BSD 3-clause",
     python_requires=">=3.8",
     install_requires=[
-        "numpy>=1.19.5",
-        "pandas>=1.3.4",
-        "scipy>=1.7.1",
-        "matplotlib>=3.4.3",
-        "seaborn>=0.11.2",
-        "h5py>=3.1.0",
-        "scikit-learn>=1.0",
-        "scikit-image>=0.18.3"
+        "numpy",
+        "pandas",
+        "scipy",
+        "matplotlib",
+        "seaborn",
+        "h5py",
+        "scikit-learn",
+        "scikit-image"
     ],
     classifiers=[
         "License :: OSI Approved :: BSD License",
